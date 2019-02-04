05217cdaabdc0316706478c801da0dbee4238d03
damirbulich <dbulic98@gmail.com>
2019-02-04 01:01:22 +0100

    Possibly got worse... need to fix mutate function!!

--- AiBlotto.java
+++ AiBlotto.java
@@ -8,66 +8,98 @@ public class AiBlotto{
 
     public static void main(String[] args){
 
-        int brGen = 10;             //broj generacija u evoluciji
+        int brGen = 20;             //broj generacija u evoluciji
 
-        int brIgraca = 40;          // broj igraca po generaciji
-        int brKula = 10;             // broj kula u igri
-        int ukBrVojnika = 100;       // broj vojnika s kojim igrac raspolaze
+        int brIgraca = 5000;          // broj igraca po generaciji
+
+        //int[] kule = {25,14,8,3,1,1,3,8,14,25}; 
+        //int[] kule = {10,0,0,0,10};  
+        //int[] kule = {10,10,10,10,10,10,10,10,10,10};
+        int[] kule = {10,10,10,10,10,10};          
+                                     // kule u igri
+
+        int ukBrVojnika = 120;       // broj vojnika s kojim igrac raspolaze
 
 
 /* 
 
-Stvaramo generaciju od 100 igraca koji se medusobno bore
+Stvaramo generaciju od |brIgraca| igraca koji se medusobno bore
 
 */
 
         List<Player> generacija = new ArrayList<>();
+        /*generacija.add(new Player());
+        generacija.get(0).addKule(brKula);
+        generacija.get(0).addVojnici(0, 2);
+        generacija.get(0).addVojnici(1, 31);
+        generacija.get(0).addVojnici(2, 31);
+        generacija.get(0).addVojnici(3, 31);
+        generacija.get(0).addVojnici(4, 23);
+        generacija.get(0).addVojnici(5, 2);
+        System.out.println(generacija.get(0).getStrategija());*/
+        for (int i=0;i<brIgraca;i++){
+            generacija.add(new Player());
+            generacija.get(i).addKule(kule);
+            generacija.get(i).calcRnStrategy(ukBrVojnika);
+            //System.out.println(generacija.get(i).getStrategija());
+        }
 
 
-        //for (int g=1; g<=brGen; g++){
+        for (int g=1; g<=brGen; g++){
 
 
-            for (int i=0;i<brIgraca;i++){
-                generacija.add(new Player());
-                generacija.get(i).addKule(brKula);
-                generacija.get(i).calcRnStrategy(ukBrVojnika);
-                //System.out.println(generacija.get(i).getStrategija());
-            }
+            
 
-            for (int i=0;i<brIgraca;i++){
+            for (int i=0;i<brIgraca-1;i++){
                 for (int j=i+1;j<brIgraca;j++){
                     int pobj1=0,pobj2=0;
-                    for (int k=0;k<brKula;k++){
+                    for (int k=0;k<kule.length;k++){
                         if (generacija.get(i).getVojniciFromKula(k)>generacija.get(j).getVojniciFromKula(k))
-                            pobj1+=2;
+                            pobj1+=generacija.get(i).getBodoviFromKula(k);
                         else if (generacija.get(i).getVojniciFromKula(k)<generacija.get(j).getVojniciFromKula(k))
-                            pobj2+=2;
-                        else{
-                            pobj1++;
-                            pobj2++;
+                            pobj2+=generacija.get(i).getBodoviFromKula(k);
+                        else if (generacija.get(i).getVojniciFromKula(k)==generacija.get(j).getVojniciFromKula(k)){
+                            pobj1+=generacija.get(i).getBodoviFromKula(k)/2;
+                            pobj2+=generacija.get(i).getBodoviFromKula(k)/2;
                         }
                     }
                     if (pobj1>pobj2)
-                        generacija.get(i).addScore(2);
+                        generacija.get(i).addScore(pobj1);
                     else if (pobj1<pobj2)
-                        generacija.get(j).addScore(2);
+                        generacija.get(j).addScore(pobj2);
                     else{
-                        generacija.get(i).addScore(1);
-                        generacija.get(j).addScore(1);
+                        generacija.get(i).addScore(pobj1);
+                        generacija.get(j).addScore(pobj2);
                     }
                 }
             }
+
+
+
             Player najbolji = new Player();
             for (int i=0; i<brIgraca; i++){
                 if (generacija.get(i).getScore()>najbolji.getScore()){
                     najbolji=generacija.get(i);
                 }
             }
-            System.out.println("Najbolji: "+najbolji.getStrategija());
+            System.out.println("Gen: "+g+"  Najbolji: "+najbolji.getStrategija());
 
+
+            generacija.clear();
+            najbolji.resetScore();
+            generacija.add(najbolji);
+
+
+            for (int i=1;i<brIgraca;i++){
+                generacija.add(new Player());
+                generacija.get(i).addKule(kule);
+                generacija.get(i).mutate(najbolji, ukBrVojnika);;
+                //System.out.println(generacija.get(i).getStrategija());
+            }
+            najbolji=null;
 
 
         }
 
-   // }
+    }
 }
--- Player.java
+++ Player.java
@@ -4,22 +4,25 @@ import java.util.Collections;
 import java.util.Random;
 
 class Player{
+    Random rn = new Random();
     private List<Kula> strategija = new ArrayList<>();
     private int score = 0;
 
-    void addKule(int brKula){
-        for (int i=0;i<brKula;i++){
-            this.strategija.add(new Kula(10));
+    void addKule(int[] bodovanje){
+        for (int i=0;i<bodovanje.length;i++){
+            this.strategija.add(new Kula(bodovanje[i]));
         }
     }
     void addVojnici(int brKule,int brVojnika){
         this.strategija.get(brKule).putVojnici(brVojnika);
     }
+    void resetScore(){
+        this.score=0;
+    }
     void addScore(int bod){
         this.score+=bod;
     }
     void calcRnStrategy(int ukBrVojnika){
-        Random rn = new Random();
         int i;
         for(i=0;i<this.strategija.size()-1;i++){
             int tempVojnika = rn.nextInt(ukBrVojnika);
@@ -27,9 +30,23 @@ class Player{
             ukBrVojnika-=tempVojnika;
         }
         this.strategija.get(i).putVojnici(ukBrVojnika);
+        //Collections.shuffle(this.strategija);
+    }
+    void mutate(Player najbolji, int ukBrVojnika){
+        int i;
+        for(i=0;i<this.strategija.size()-1;i++){
+            int tempVojnika = rn.nextInt(ukBrVojnika);
+            this.strategija.get(i).putVojnici(tempVojnika);
+            ukBrVojnika-=tempVojnika;
+        }
+        this.strategija.get(i).putVojnici(ukBrVojnika);
+        //Collections.shuffle(this.strategija);
     }
     int getVojniciFromKula(int brKule){
         return this.strategija.get(brKule).getVojnici();
+    }
+    int getBodoviFromKula(int k){
+        return this.strategija.get(k).getBodovi();
     }
     int getScore(){
         return this.score;
